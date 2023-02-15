b3108bb5df5010193b06e86267667afdda222744
Bui The Luan <buitheluan@HN-MacOS-LuanBui.local>
2023-02-16 01:24:26 +0700

    update

---
+++ app/app/calc.py
@@ -0,0 +1,8 @@
+""" Calculator fuctions """
+
+def add(x,y):
+    """ Add x, y andf return result."""
+    return x + y
+def subtract(x,y):
+    """ Subtract x from y and return result."""
+    return y - x    
